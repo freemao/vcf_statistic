47312f727fc42f5485c4d1a19cdb2c1774360e84
393099545@qq.com <cmiao@apple.(none)>
2014-08-12 17:08:31 +0800

    update

--- vcf_statistic.py
+++ vcf_statistic.py
@@ -1,40 +1,50 @@
 #!/usr/lib/python
 #-*- coding:utf-8 -*-
+from optparse import OptionParser
 
-def GenoType_statistic(*names):
+msg_usage = 'usage: %prog [-M] geno, var [-F] vcffile'
+descr ='''count the genotype and variant type in each seq
+ '''
+optparser = OptionParser(usage = msg_usage, description = descr)
+optparser.add_option('-M', '--model', dest = 'model',type = 'string',
+                       help = 'count genotype or variant type.')
+optparser.add_option('-F', '--file', dest = 'vcffile',type = 'string',
+                       help = 'count genotype or variant type.')
+options, args = optparser.parse_args()
+
+def GenoType_statistic(vcfname):
     '''Through statistic by this function, you will get a file which contain
     each files' total snp, the number of 0/1, 0/0, and 1/1.
     '''
     f0 = open('genotype_results.txt', 'w')
     f0.write('samples\t0/0\t0/1\t1/1\tothers\ttotal\n')
-    for fn in names:
-        sm = '.'.join(fn.split('.')[0:-1])
-        f1 = open(fn, 'r')
-        total = 0
-        zerzer = 0
-        zerone = 0
-        oneone = 0
-        others = 0
-        for i in f1:
-            if i.startswith('#'):
-                pass
+    sm = '.'.join(vcfname.split('.')[0:-1])
+    f1 = open(fn, 'r')
+    total = 0
+    zerzer = 0
+    zerone = 0
+    oneone = 0
+    others = 0
+    for i in f1:
+        if i.startswith('#'):
+            pass
+        else:
+            total += 1
+            j = i.split()[9].split(':')[0]
+            if j == '0/0':
+                zerzer += 1
+            elif j == '0/1':
+                zerone += 1
+            elif j == '1/1':
+                oneone += 1
             else:
-                total += 1
-                j = i.split()[9].split(':')[0]
-                if j == '0/0':
-                    zerzer += 1
-                elif j == '0/1':
-                    zerone += 1
-                elif j == '1/1':
-                    oneone += 1
-                else:
-                    others += 1
-        f1.close()
-        f0.write(sm + '\t' + str(zerzer) + '\t' + str(zerone) + '\t' + \
+                others += 1
+    f1.close()
+    f0.write(sm + '\t' + str(zerzer) + '\t' + str(zerone) + '\t' + \
 str(oneone) + '\t' + str(others) + '\t' + str(total) + '\n')
     f0.close()
 
-def VariantType_fb_statistic(*names):
+def VariantType_fb_statistic(vcfname):
     from VCF_Parser import FbVcf
     '''This function is designed for freebayes, you will get a file which
     contain each vcf files' total variant type, snp, ins, del, mnp, complex
@@ -43,100 +53,102 @@ def VariantType_fb_statistic(*names):
     '''
     f0 = open('FB.VariantType.results.txt', 'w')
     f0.write('samples\tSNP\tINS\tDEL\tMNP\tCOMPLEX\tOTHERS\tTOTAL\n')
-    for fn in names:
-        sm = '.'.join(fn.split('.')[0:-1])
-        f1 = open(fn, 'r')
-        Ttotal, Tsnp, Tins, Tdeletion, Tmnp, Tcomplex, Tothers = \
+    sm = '.'.join(fn.split('.')[0:-1])
+    f1 = open(vcfname, 'r')
+    Ttotal, Tsnp, Tins, Tdeletion, Tmnp, Tcomplex, Tothers = \
 0, 0, 0, 0, 0, 0, 0
-        Etotal, Esnp, Eins, Edeletion, Emnp, Ecomplex, Eothers = \
+    Etotal, Esnp, Eins, Edeletion, Emnp, Ecomplex, Eothers = \
 0, 0, 0, 0, 0, 0, 0
-        chrlist = []
-        for i in f1:
-            if i.startswith('#'):
-                pass
-            else:
-                Ttotal += 1
-                instance1 = FbVcf()
-                Chr = instance1.chr(i)
-                vartype = instance1.VariantType(i)
-                if Chr not in chrlist and len(chrlist)==0:
-                    '''if vartype == 'snp':
-                        Tsnp += 1
-                        Esnp += 1
-                    elif vartype == 'ins':
-                        Tins += 1
-                        Eins += 1
-                    elif vartype == 'del':
-                        Tdeletion += 1
-                        Edeletion += 1
-                    elif vartype == 'mnp':
-                        Tmnp += 1
-                        Emnp += 1
-                    elif vartype == 'complex':
-                        Tcomplex += 1
-                        Ecomplex += 1
-                    else :
-                        Tothers += 1
-                        Eothers += 1
-                    Etotal += 1'''
-                    chrlist.append(Chr)
-                if Chr in chrlist:
-                    if vartype == 'snp':
-                        Tsnp += 1
-                        Esnp += 1
-                    elif vartype == 'ins':
-                        Tins += 1
-                        Eins += 1
-                    elif vartype == 'del':
-                        Tdeletion += 1
-                        Edeletion += 1
-                    elif vartype == 'mnp':
-                        Tmnp += 1
-                        Emnp += 1
-                    elif vartype == 'complex':
-                        Tcomplex += 1
-                        Ecomplex += 1
-                    else :
-                        Tothers += 1
-                        Eothers += 1
-                    Etotal += 1
-                if Chr not in chrlist and len(chrlist)!=0:
-                    f0.write(chrlist[-1] + '\t' + str(Esnp) + '\t' + str(Eins) + '\t' + \
+    chrlist = []
+    for i in f1:
+        if i.startswith('#'):
+            pass
+        else:
+            Ttotal += 1
+            instance1 = FbVcf()
+            Chr = instance1.chr(i)
+            vartype = instance1.VariantType(i)
+            if Chr not in chrlist and len(chrlist)==0:
+                '''if vartype == 'snp':
+                    Tsnp += 1
+                    Esnp += 1
+                elif vartype == 'ins':
+                    Tins += 1
+                    Eins += 1
+                elif vartype == 'del':
+                    Tdeletion += 1
+                    Edeletion += 1
+                elif vartype == 'mnp':
+                    Tmnp += 1
+                    Emnp += 1
+                elif vartype == 'complex':
+                    Tcomplex += 1
+                    Ecomplex += 1
+                else :
+                    Tothers += 1
+                    Eothers += 1
+                Etotal += 1'''
+                chrlist.append(Chr)
+            if Chr in chrlist:
+                if vartype == 'snp':
+                    Tsnp += 1
+                    Esnp += 1
+                elif vartype == 'ins':
+                    Tins += 1
+                    Eins += 1
+                elif vartype == 'del':
+                    Tdeletion += 1
+                    Edeletion += 1
+                elif vartype == 'mnp':
+                    Tmnp += 1
+                    Emnp += 1
+                elif vartype == 'complex':
+                    Tcomplex += 1
+                    Ecomplex += 1
+                else :
+                    Tothers += 1
+                    Eothers += 1
+                Etotal += 1
+            if Chr not in chrlist and len(chrlist)!=0:
+                f0.write(chrlist[-1] + '\t' + str(Esnp) + '\t' + str(Eins) + '\t' + \
 str(Edeletion) + '\t' + str(Emnp)+'\t'+str(Ecomplex)+'\t'+str(Eothers) \
 + '\t' + str(Etotal) + '\n')
-                    Esnp, Eins, Edeletion, Emnp, Ecomplex, Eothers, Etotal =\
+                Esnp, Eins, Edeletion, Emnp, Ecomplex, Eothers, Etotal =\
 0, 0, 0, 0, 0, 0, 0
-                    if vartype == 'snp':
-                        Tsnp += 1
-                        Esnp += 1
-                    elif vartype == 'ins':
-                        Tins += 1
-                        Eins += 1
-                    elif vartype == 'del':
-                        Tdeletion += 1
-                        Edeletion += 1
-                    elif vartype == 'mnp':
-                        Tmnp += 1
-                        Emnp += 1
-                    elif vartype == 'complex':
-                        Tcomplex += 1
-                        Ecomplex += 1
-                    else :
-                        Tothers += 1
-                        Eothers += 1
-                    Etotal += 1
-                    chrlist.append(Chr)
+                if vartype == 'snp':
+                    Tsnp += 1
+                    Esnp += 1
+                elif vartype == 'ins':
+                    Tins += 1
+                    Eins += 1
+                elif vartype == 'del':
+                    Tdeletion += 1
+                    Edeletion += 1
+                elif vartype == 'mnp':
+                    Tmnp += 1
+                    Emnp += 1
+                elif vartype == 'complex':
+                    Tcomplex += 1
+                    Ecomplex += 1
+                else :
+                    Tothers += 1
+                    Eothers += 1
+                Etotal += 1
+                chrlist.append(Chr)
 
-        f1.close()
-        f0.write(chrlist[-1] + '\t' + str(Esnp) + '\t' + str(Eins) + '\t' + \
+    f1.close()
+    f0.write(chrlist[-1] + '\t' + str(Esnp) + '\t' + str(Eins) + '\t' + \
 str(Edeletion) + '\t' + str(Emnp)+'\t'+str(Ecomplex)+'\t'+str(Eothers) +\
  '\t' + str(Etotal) + '\n\n')
-        f0.write(sm + '\t' + str(Tsnp) + '\t' + str(Tins) + '\t' + \
+    f0.write(sm + '\t' + str(Tsnp) + '\t' + str(Tins) + '\t' + \
 str(Tdeletion) + '\t' + str(Tmnp)+'\t'+str(Tcomplex)+'\t'+str(Tothers) \
 + '\t' + str(Ttotal) + '\n')
     f0.close()
 
 if __name__ == '__main__':
-    import sys
-    argslist = sys.argv[1:]
-    VariantType_fb_statistic(*sys.argv[1:])
+    m = options.model
+    f = options.vcffile
+    if m == 'geno':
+        GenoType_statistic(f)
+    if m == 'var':
+        VariantType_fb_statistic(f)
